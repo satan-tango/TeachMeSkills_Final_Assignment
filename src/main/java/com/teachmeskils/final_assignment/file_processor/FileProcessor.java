f2fc98aef79d615200730656e94ca15f08ed97c3
Igor Abramovich <igorab2033@gmail.com>
2024-11-28 20:52:57 +0300

    igor commit #2

--- src/main/java/com/teachmeskils/final_assignment/file_processor/FileProcessor.java
+++ src/main/java/com/teachmeskils/final_assignment/file_processor/FileProcessor.java
@@ -22,38 +22,40 @@ public class FileProcessor {
                 File[] subDirectories = directory.listFiles();
                 Map<String, List<File>> map = new HashMap<>();
 
-                for (File subDirectory : subDirectories) {
-                    if (subDirectory.isDirectory()) {
-                        File[] files = subDirectory.listFiles();
-                        List<File> checkList = new ArrayList<>();
-                        List<File> invoiceList = new ArrayList<>();
-                        List<File> orderList = new ArrayList<>();
-                        if (files != null) {
-                            for (File file : files) {
-                                if (file.isFile() && file.getName().contains("2024") && file.getName().endsWith(".txt")) {
-                                    if (subDirectory.getName().contains("checks")) {
-                                        checkList.add(file);
-                                        map.put(subDirectory.getName(), checkList);
-//                                            Path path = Paths.get(file.getAbsolutePath());
-//                                            Path destPath = Paths.get(Constant.VALID_CHECK_PATH + File.separator + file.getName());
-//                                            Files.copy(path, destPath, StandardCopyOption.REPLACE_EXISTING);
-                                    } else if (subDirectory.getName().contains("invoices")) {
-                                        invoiceList.add(file);
-                                        map.put(subDirectory.getName(), invoiceList);
-//                                            Path path = Paths.get(file.getAbsolutePath());
-//                                            Path destPath = Paths.get(Constant.VALID_INVOICE_PATH + File.separator + file.getName());
-//                                            Files.copy(path, destPath, StandardCopyOption.REPLACE_EXISTING);
-                                    } else if (subDirectory.getName().contains("orders")) {
-                                        orderList.add(file);
-                                        map.put(subDirectory.getName(), orderList);
-//                                            Path path = Paths.get(file.getAbsolutePath());
-//                                            Path destPath = Paths.get(Constant.VALID_ORDERS_PATH + File.separator + file.getName());
-//                                            Files.copy(path, destPath, StandardCopyOption.REPLACE_EXISTING);
+                if (subDirectories != null) {
+                    for (File subDirectory : subDirectories) {
+                        if (subDirectory.isDirectory()) {
+                            File[] files = subDirectory.listFiles();
+                            List<File> checkList = new ArrayList<>();
+                            List<File> invoiceList = new ArrayList<>();
+                            List<File> orderList = new ArrayList<>();
+                            if (files != null) {
+                                for (File file : files) {
+                                    if (file.isFile() && file.getName().contains("2024") && file.getName().endsWith(".txt")) {
+                                        if (subDirectory.getName().contains("checks")) {
+                                            checkList.add(file);
+                                            map.put(subDirectory.getName(), checkList);
+    //                                            Path path = Paths.get(file.getAbsolutePath());
+    //                                            Path destPath = Paths.get(Constant.VALID_CHECK_PATH + File.separator + file.getName());
+    //                                            Files.copy(path, destPath, StandardCopyOption.REPLACE_EXISTING);
+                                        } else if (subDirectory.getName().contains("invoices")) {
+                                            invoiceList.add(file);
+                                            map.put(subDirectory.getName(), invoiceList);
+    //                                            Path path = Paths.get(file.getAbsolutePath());
+    //                                            Path destPath = Paths.get(Constant.VALID_INVOICE_PATH + File.separator + file.getName());
+    //                                            Files.copy(path, destPath, StandardCopyOption.REPLACE_EXISTING);
+                                        } else if (subDirectory.getName().contains("orders")) {
+                                            orderList.add(file);
+                                            map.put(subDirectory.getName(), orderList);
+    //                                            Path path = Paths.get(file.getAbsolutePath());
+    //                                            Path destPath = Paths.get(Constant.VALID_ORDERS_PATH + File.separator + file.getName());
+    //                                            Files.copy(path, destPath, StandardCopyOption.REPLACE_EXISTING);
+                                        }
                                     }
                                 }
+                            } else {
+                                System.out.println("В директории нет файлов.");
                             }
-                        } else {
-                            System.out.println("В директории нет файлов.");
                         }
                     }
                 }
