381afaf819a9532c18e5731fdead561e259e6be4
Dmitry <d.varkov123darg@gmail.com>
2024-12-07 14:55:45 +0300

    Bugs has been fixed
    Took 23 minutes

--- src/main/java/com/teachmeskils/final_assignment/ApplicationRunner.java
+++ src/main/java/com/teachmeskils/final_assignment/ApplicationRunner.java
@@ -2,7 +2,7 @@ package com.teachmeskils.final_assignment;
 
 import com.teachmeskils.final_assignment.execption.NumberNotFoundException;
 import com.teachmeskils.final_assignment.execption.TotalLineNotFoundException;
-import com.teachmeskils.final_assignment.operations.FileProcessor;
+import com.teachmeskils.final_assignment.operations.FileOperation;
 import com.teachmeskils.final_assignment.service.CalculationTotalTurnover;
 
 import java.io.File;
@@ -13,7 +13,7 @@ import java.util.Map;
 public class ApplicationRunner {
 
     public static void main(String[] args) throws NumberNotFoundException, TotalLineNotFoundException, IOException {
-        Map<String, List<File>> documents = FileProcessor.convertListOfFilesToMap();
+        Map<String, List<File>> documents = FileOperation.convertListOfFilesToMap();
         CalculationTotalTurnover.reportCalculationTotalTurnover(documents);
     }
 }
--- src/main/java/com/teachmeskils/final_assignment/operations/FileOperation.java
+++ src/main/java/com/teachmeskils/final_assignment/operations/FileOperation.java
@@ -5,7 +5,7 @@ import com.teachmeskils.final_assignment.constants.Constants;
 import java.io.*;
 import java.util.*;
 
-public class FileProcessor {
+public class FileOperation {
 
     public static File gettingFolderPath() {
         try (Scanner scanner = new Scanner(System.in)) {
@@ -17,44 +17,42 @@ public class FileProcessor {
     public static Map<String, List<File>> convertListOfFilesToMap() {
         //File directory = gettingFolderPath();
         File directory = new File(Constants.PATH_DOCUMENTS);
+
         if (!directory.exists() || !directory.isDirectory()) {
             System.out.println("Указанная директория не существует или это не папка.");
         }
+
         Map<String, List<File>> map = new HashMap<>();
         List<File> checkList = new ArrayList<>();
         List<File> invoiceList = new ArrayList<>();
         List<File> orderList = new ArrayList<>();
-        try {
-            File[] subDirectories = directory.listFiles();
-            if (subDirectories != null) {
-                for (File subDirectory : subDirectories) {
-                    if (subDirectory.isDirectory()) {
-                        File[] files = subDirectory.listFiles();
-                        if (files != null) {
-                            for (File file : files) {
-                                if (file.isFile() && file.getName().contains("2024") && file.getName().endsWith(".txt")) {
-                                    if (file.getName().toLowerCase().contains("bill")) {
-                                        checkList.add(file);
-                                    } else if (file.getName().toLowerCase().contains("invoice")) {
-                                        invoiceList.add(file);
-                                    } else if (file.getName().toLowerCase().contains("order")) {
-                                        orderList.add(file);
-                                    }
+        File[] subDirectories = directory.listFiles();
+        if (subDirectories != null) {
+            for (File subDirectory : subDirectories) {
+                if (subDirectory.isDirectory()) {
+                    File[] files = subDirectory.listFiles();
+                    if (files != null) {
+                        for (File file : files) {
+                            if (file.isFile() && file.getName().contains("2024") && file.getName().endsWith(".txt")) {
+                                if (file.getName().toLowerCase().contains("bill")) {
+                                    checkList.add(file);
+                                } else if (file.getName().toLowerCase().contains("invoice")) {
+                                    invoiceList.add(file);
+                                } else if (file.getName().toLowerCase().contains("order")) {
+                                    orderList.add(file);
                                 }
                             }
-                        } else {
-                            System.out.println("В директории нет файлов.");
                         }
+                    } else {
+                        System.out.println("В директории нет файлов.");
                     }
                 }
             }
-            map.put("checks", checkList);
-            map.put("invoices", invoiceList);
-            map.put("orders", orderList);
-        } catch (Exception e) {
-            System.out.println("Произошла ошибка при чтении файлов: " + e.getMessage());
         }
-       return  map;
+        map.put("checks", checkList);
+        map.put("invoices", invoiceList);
+        map.put("orders", orderList);
+        return  map;
     }
 //    public static List<File> convertListOfFilesToMap12() {
 //        File directory = gettingFolderPath();
