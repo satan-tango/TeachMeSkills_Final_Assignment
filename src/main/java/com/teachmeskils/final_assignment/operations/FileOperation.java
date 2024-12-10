13441b34029496fbe3aa8f84371af6dfd5283171
Igor Abramovich <igorab2033@gmail.com>
2024-12-10 22:44:43 +0300

    Igor commit #7

--- src/main/java/com/teachmeskils/final_assignment/operations/FileOperation.java
+++ src/main/java/com/teachmeskils/final_assignment/operations/FileOperation.java
@@ -14,6 +14,7 @@ public class FileOperation {
 
     public static void copyFile(Path sourcePath, Path destPath) {
         try {
+            Logger.logInfo("Copied " + sourcePath + " to " + destPath);
             Files.copy(sourcePath, destPath, StandardCopyOption.REPLACE_EXISTING);
         } catch (IOException e) {
             Logger.logException(e);
@@ -21,44 +22,46 @@ public class FileOperation {
     }
 
     public static Map<String, List<File>> fileOperation(){
-        Logger.logInfo("Работа с папкой начата");
         //File directory = gettingFolderPath();
         File directory = new File(Constants.PATH_DOCUMENTS);
-        Logger.logInfo("Получение папки в работу");
+        Logger.logInfo("Получение папки " + directory.getName() + " в работу");
 
         if (!directory.exists() || !directory.isDirectory()) {
             System.out.println("Указанная директория не существует или это не папка.");
         }
-        Logger.logInfo("Получение подпапки в работу");
-
         Map<String, List<File>> map = new HashMap<>();
         List<File> checkList = new ArrayList<>();
         List<File> invoiceList = new ArrayList<>();
         List<File> orderList = new ArrayList<>();
         File[] subDirectories = directory.listFiles();
-        Logger.logInfo("Получение файлов в работу");
-        Logger.logInfo("Работа с файлами начата");
         if (subDirectories != null) {
             for (File subDirectory : subDirectories) {
+                Logger.logInfo("Получение подпапки " + subDirectory.getName() + " в работу");
                 if (subDirectory.isDirectory()) {
                     File[] files = subDirectory.listFiles();
                     if (files != null) {
                         for (File file : files) {
                             if (file.isFile() && file.getName().contains("2024") && file.getName().endsWith(".txt") && file.getName().toLowerCase().contains("bill")) {
+                                    Logger.logInfo("Работа с файлом " + file.getName());
                                     checkList.add(file);
                                     copyFile(Paths.get(file.getAbsolutePath()), Paths.get(Constants.VALID_CHECK_PATH + File.separator + file.getName()));
                             } else if (file.isFile() && file.getName().contains("2024") && file.getName().endsWith(".txt") && file.getName().toLowerCase().contains("invoice")) {
+                                    Logger.logInfo("Работа с файлом " + file.getName());
                                     invoiceList.add(file);
                                     copyFile(Paths.get(file.getAbsolutePath()), Paths.get(Constants.VALID_INVOICE_PATH + File.separator + file.getName()));
                             } else if (file.isFile() && file.getName().contains("2024") && file.getName().endsWith(".txt") && file.getName().toLowerCase().contains("order")) {
+                                    Logger.logInfo("Работа с файлом " + file.getName());
                                     orderList.add(file);
                                     copyFile(Paths.get(file.getAbsolutePath()), Paths.get(Constants.VALID_ORDERS_PATH + File.separator + file.getName()));
                             }else {
                                 if (subDirectory.getName().toLowerCase().contains("checks")) {
+                                    Logger.logInfo("Работа с файлом " + file.getName());
                                     copyFile(Paths.get(file.getAbsolutePath()), Paths.get(Constants.INVALID_CHECK_PATH + File.separator + file.getName()));
                                 }else if (subDirectory.getName().toLowerCase().contains("invoices")) {
+                                    Logger.logInfo("Работа с файлом " + file.getName());
                                     copyFile(Paths.get(file.getAbsolutePath()), Paths.get(Constants.INVALID_INVOICE_PATH + File.separator + file.getName()));
                                 }else if (subDirectory.getName().toLowerCase().contains("orders")) {
+                                    Logger.logInfo("Работа с файлом " + file.getName());
                                     copyFile(Paths.get(file.getAbsolutePath()), Paths.get(Constants.INVALID_ORDERS_PATH + File.separator + file.getName()));
                                 }
                             }
@@ -69,15 +72,10 @@ public class FileOperation {
                 }
             }
         }
-        Logger.logInfo("Валидные файлы скопированы в папку valid_data");
-        Logger.logInfo("Невалидные файлы скопированы в папку invalid_data");
-        Logger.logInfo("Работа с попдпапкой закончена");
-        Logger.logInfo("Работа с папкой закончена");
         map.put("checks", checkList);
         map.put("invoices", invoiceList);
         map.put("orders", orderList);
         Logger.logInfo("Валидные файлы занесены в map");
-        Logger.logInfo("Работа с файлами закончена");
         Logger.logInfo("________________________________");
         return  map;
     }
